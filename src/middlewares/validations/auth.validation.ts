0494895cdcf7dc211548ac0b61e09beca004a356
YuriiVladimirovichPopov <papanchik87@yahoo.com>
2023-09-04 22:05:06 +0500

    fix 5

--- src/middlewares/validations/auth.validation.ts
+++ src/middlewares/validations/auth.validation.ts
@@ -4,6 +4,7 @@ import { sendStatus } from "../../routers/send-status";
 import { usersCollection } from "../../db/db";
 import { UsersMongoDbType } from '../../types';
 import { UserViewModel } from '../../models/users/userViewModel';
+import { ObjectId } from 'mongodb';
 
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
@@ -17,15 +18,18 @@ const token = req.headers.authorization.split(' ')[1]
 const userId = await jwtService.getUserIdByToken(token)
 
 if (!userId) {
-    res.sendStatus(401)
+    res.sendStatus(sendStatus.UNAUTHORIZED_401)
     return  
 }
-
-const user = await usersCollection.findOne({id: userId})
+if (!ObjectId.isValid(userId)) {
+return res.sendStatus(sendStatus.UNAUTHORIZED_401)
+}
+const user = await usersCollection.findOne({_id: new ObjectId(userId)})
 
         if (!user) {
         return res.sendStatus(sendStatus.UNAUTHORIZED_401)
         }
+        
 const mappedUser: UserViewModel = {
     id: user._id.toString(),
     login: user.login,
